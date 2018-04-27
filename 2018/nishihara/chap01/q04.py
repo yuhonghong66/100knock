68ac6b8bc781aa8190a2fbc97a82fff4c04f896c
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2018-04-27 15:31:51 +0900

    [fix] (04) 出力をlistからdictに変更

--- 2018/nishihara/chap01/q04.py
+++ 2018/nishihara/chap01/q04.py
@@ -1,4 +1,4 @@
 string = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."
 nums = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 
-print(list(map(lambda x: (x[1][:1 if x[0]+1 in nums else 2], x[0]), enumerate(string.replace(".", "").split(" ")))))
+print(dict(map(lambda x: (x[1][:1 if x[0]+1 in nums else 2], x[0]), enumerate(string.replace(".", "").split(" ")))))
