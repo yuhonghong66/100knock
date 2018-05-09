ef0a036cacd50aeb3385a476ee02dd686c32232f
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2018-05-09 13:30:39 +0900

    [add] (05) 完成

---
+++ 2018/nishihara/chap01/q05.py
@@ -0,0 +1,10 @@
+def ngram(tlist, n):
+    l = []
+    for i in range(len(tlist) - n + 1):
+        l.append(tlist[i:i+n])
+    return l
+
+
+str = "I am an NLPer"
+print(ngram(str.split(" "), 2))
+print(ngram(list(str), 2))
