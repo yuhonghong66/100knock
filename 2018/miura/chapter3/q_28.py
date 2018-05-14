d0d6bee7af891056080c3b16df7b09a5cf38e121
miura <miura.biwa@ist.osaka-u.ac.jp>
2018-05-15 02:19:33 +0900

    add q_{27,28}.py

---
+++ 2018/miura/chapter3/q_28.py
@@ -0,0 +1,12 @@
+from q_20 import extract
+import re
+
+basic_dict = {}
+for line in extract('イギリス').split('\n'):
+    if re.match('^\|.* = .*', line):
+        s = re.sub(r'<.*>', '', line.replace('|','').replace('[[','').replace(']]','').replace('{{','').replace('}}','').replace("'",''))
+        word = re.split(' = ', s)
+        basic_dict[word[0]] = word[1]
+
+for k,v in basic_dict.items():
+    print(k + ' : ' + v)
