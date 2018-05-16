52fc60dd517e3074ce6dd7344efdd3e94cdeb2e0
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2018-05-17 07:11:40 +0900

    [update] (25) 他の問題用に整理

--- 2018/nishihara/chap03/q25.py
+++ 2018/nishihara/chap03/q25.py
@@ -1,9 +1,11 @@
 import re
 import q20
 
+def basicinfo(string):
+    s = re.search("{{[^}]*基礎情報[^{}]*({{[^{}]*}}[^{}]*)*}}",\
+        string, flags=re.DOTALL).group()
+    return dict([tuple(re.split(" *= *", l[1:], 1)) for l in s.split("\n") if "=" in l])
 
 if __name__ == '__main__':
     string = q20.picktxt("../../../data/jawiki-country.json", "イギリス")
-    s = re.search("{{[^}]*基礎情報[^{}]*({{[^{}]*}}[^{}]*)*}}",\
-        string, flags=re.DOTALL).group()
-    print(dict([tuple(re.split(" *= *", l[1:], 1)) for l in s.split("\n") if "=" in l]))
+    print(basicinfo(string))
