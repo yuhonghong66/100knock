fa297461daa398721fde413be769744335614b19
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2018-05-18 09:59:03 +0900

    [update] (30) 他の問題用に改変

--- 2018/nishihara/chap04/q30.py
+++ 2018/nishihara/chap04/q30.py
@@ -3,15 +3,20 @@
 # 形態素解析
 # $ mecab -F"%m,%f[6],%f[1],%f[2]\n" neko.txt > neko.txt.mecab
 
-tpl = ["surface", "base", "pos", "pos1"]
 
-rst = []
-ln = []
-with open("../../../data/neko.txt.mecab", "r") as f:
-    for l in f.readlines():
-        if("EOS" in l):
-            rst.append(ln)
-            ln = []
-        else:
-            ln.append(dict(zip(tpl, l.rstrip().split(","))))
-    print(rst)
+def readma(path):
+    tpl = ["surface", "base", "pos", "pos1"]
+
+    rst = []
+    ln = []
+    with open(path, "r") as f:
+        for l in f.readlines():
+            if("EOS" in l):
+                rst.append(ln)
+                ln = []
+            else:
+                ln.append(dict(zip(tpl, l.rstrip().split(","))))
+    return rst
+
+if __name__ == '__main__':
+    print(readma("../../../data/neko.txt.mecab"))
