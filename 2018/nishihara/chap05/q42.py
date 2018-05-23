ee1a88afd953c869976b51fe8b8b47c2ba77a00d
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2018-05-23 16:25:00 +0900

    [update] クラスメソッド追加をクラス定義へ移動

--- 2018/nishihara/chap05/q42.py
+++ 2018/nishihara/chap05/q42.py
@@ -1,17 +1,5 @@
-import re
 from q40 import Morph
 from q41 import Chunk, getChunks
-
-
-def add_instatnce(cls, method):
-    setattr(cls, method.__name__, method)
-
-def get_surface(self):
-    return "".join([re.sub("[。、,.]", "", m.surface) for m in self.morphs])
-
-add_instatnce(Chunk, get_surface)
-
-
 
 
 if __name__ == '__main__':
