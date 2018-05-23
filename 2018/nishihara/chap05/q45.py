85ad20547393417af861e1029e1b8108bb2dca44
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2018-05-24 00:58:17 +0900

    [add] (45) 完成

---
+++ 2018/nishihara/chap05/q45.py
@@ -0,0 +1,23 @@
+from q41 import getChunks
+from collections import defaultdict
+
+if __name__ == '__main__':
+    rst = getChunks("../../../data/neko.txt.cabocha")
+
+    for s in rst[:8]:
+        for c in s:
+            v = ""
+            p = []
+            for m in c.morphs:
+                if(m.pos == "動詞"):
+                    v = m.base
+                    break
+            else:
+                continue
+            for src in c.srcs:
+                for m in s[src].morphs:
+                    if m.pos == "助詞":
+                        p.append(m.base)
+            if(len(p) > 0):
+                p.sort()
+                print(v + "\t" + "\t".join(p))
