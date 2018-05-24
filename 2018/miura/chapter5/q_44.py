d71bb70f4720f060440a3c6fa3e349a6ff021404
miura <miura.biwa@ist.osaka-u.ac.jp>
2018-05-24 20:17:04 +0900

    add q_{40-44}.py and q_40.sh

---
+++ 2018/miura/chapter5/q_44.py
@@ -0,0 +1,50 @@
+import CaboCha
+from graphviz import Digraph
+import sys
+import re
+import pydot
+
+s = input('> ') if len(sys.argv) < 2 else sys.argv[1]
+
+cabocha = CaboCha.Parser()
+lattice = cabocha.parse(s).toString(CaboCha.FORMAT_LATTICE)
+#print(lattice)
+
+#q_40.py の Chunkはリッチすぎるので簡単なのつくる
+class Chunk:
+    def __init__(self, dst, surface):
+        self.dst = dst
+        self.surface = surface
+
+allsentence = []
+chunks = []
+now_chunk = {'surface':'', 'dst':None}
+for line in lattice.split('\n'):
+    if line == 'EOS':
+        chunks.append(Chunk(**now_chunk))
+        now_chunk = {'surface':'', 'dst':None}
+        allsentence.append(chunks)
+        chunks = []
+    elif re.match(r'^\*', line):
+        if now_chunk['dst'] is not None:
+            chunks.append(Chunk(**now_chunk))
+        now_chunk = {'surface':'', 'dst':int(line.split(' ')[2][:-1])}
+    else:
+        now_chunk['surface'] += re.split(r'[,\t]', line)[0]
+
+'''
+for chunks in allsentence:
+    for chunk in chunks:
+        print('{} -> {}'.format(chunk.surface, chunks[chunk.dst].surface if chunk.dst != -1 else 'x'))
+'''
+
+#有効グラフをつくる
+G = Digraph(format='png')
+G.attr('node', shape='oval')
+for i,chunks in enumerate(allsentence):
+    for j,chunk in enumerate(chunks):
+    	G.node('{}-{}'.format(i,j), chunk.surface)
+    	if chunk.dst != -1:
+    		G.edge('{}-{}'.format(i,j), '{}-{}'.format(i,chunk.dst))
+
+G.render('graph')
