f264449f0b0bd7a9ed4905fa091d3a79cd85c027
Nishihara-Daiki <e1227@ob.akashi.ac.jp>
2018-06-02 21:26:51 +0900

    [add] (52) 完成

---
+++ 2018/nishihara/chap06/q52.py
@@ -0,0 +1,8 @@
+# python3 q50.py | python3 q51.py | python3 q52.py
+
+import sys
+from stemming.porter2 import stem
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        print(stem(line.strip()))
